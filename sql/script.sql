65a02a3f9ee11635ad09add6f83dd4df260ea985
Adailson Oliveira <adailsonramonmat@gmail.com>
2026-01-19 22:37:12 -0300

    ajuste1

--- sql/script.sql
+++ sql/script.sql
@@ -5,10 +5,13 @@ CREATE TABLE carriers (
 );
 
 CREATE TABLE phones (
-  id SERIAL PRIMARY KEY,
-  "customerId" INT NOT NULL,
+    id SERIAL PRIMARY KEY,
   number VARCHAR(20) NOT NULL UNIQUE,
-  "carrierId" INT REFERENCES carriers(id)
+  "carrierId" INT NOT NULL REFERENCES carriers(id),
+  "name" VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  "cpf" VARCHAR(11) NOT NULL,
+  "createdAt" TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE recharges (
